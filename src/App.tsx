37830bcd60421e61ad9d86badceaad1d70638da3
Keita Tanemori <ku74n1@gmail.com>
2026-03-01 15:44:18 +0900

    初回コミット: Gakushu プロジェクトセットアップ
    Vite + React + TypeScript で勉強時間・項目管理アプリの土台を構築。
    Chakra UI v3、react-icons、ESLint、Prettier を導入済み。

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Box display="flex" alignItems="center" justifyContent="center" minH="100vh">
+      <Heading size="4xl">Hello World</Heading>
+    </Box>
+  )
+}
+
+export default App
